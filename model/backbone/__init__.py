8eb8a044c24f8c2113af0881492f375efc442409
Laura Daza <la.daza10@uniandes.edu.co>
2020-04-17 16:31:47 -0500

    Update __init__.py

--- model/backbone/__init__.py
+++ model/backbone/__init__.py
@@ -1,14 +1,5 @@
-from model.backbone import resnet, xception, drn, mobilenet
+from model.backbone import xception
 
 
 def build_backbone(backbone, filters, Norm_Layer):
-    if backbone == 'resnet':
-        return resnet.ResNet101(filters, Norm_Layer)
-    elif backbone == 'xception':
-        return xception.AlignedXception(filters, Norm_Layer)
-    elif backbone == 'drn':
-        return drn.drn_d_54(filters)
-    elif backbone == 'mobilenet':
-        return mobilenet.MobileNetV2(filters)
-    else:
-        raise NotImplementedError
+    return xception.AlignedXception(filters, Norm_Layer)
